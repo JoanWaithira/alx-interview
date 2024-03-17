d410e711ecbf909b4138779961cc5f510c647b58
JoanWaithira <joanwaithira.jw@gmail.com>
2024-03-17 13:58:34 +0000

    Documenting the code

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python3
+"""Alx interview Lockboxes"""
+
+
 def canUnlockAll(boxes):
+    """
+    Determine if all boxes can be unlocked.
+
+    Args:
+        boxes (list): A list of lists representing the boxes.
+            Each inner list contains keys that correspond to other boxes.
+
+    Returns:
+        bool: True if all boxes can be opened, False otherwise.
+
+    Example:
+        boxes = [[1], [2], [3], [4], []]
+        canUnlockAll(boxes)  # Output: True
+    """
     if len(boxes) == 0:
         return False
 
