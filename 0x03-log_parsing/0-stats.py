1a61b88c5a7281f51868cb961baedc9c2465d283
JoanWaithira <joanwaithira.jw@gmail.com>
2024-03-28 11:03:05 +0000

    Log Parsing

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+import sys
+
+
+def print_message(status_codes_dict, total_file_size):
+    """
+    Method to print
+    Args:
+        status_codes_dict: dict of status codes
+        total_file_size: total of the file
+    Returns:
+        Nothing
+    """
+
+    print("File size: {}".format(total_file_size))
+    for key, val in sorted(status_codes_dict.items()):
+        if val != 0:
+            print("{}: {}".format(key, val))
+
+
+total_file_size = 0
+code = 0
+counter = 0
+status_codes_dict = {"200": 0,
+                    "301": 0,
+                    "400": 0,
+                    "401": 0,
+                    "403": 0,
+                    "404": 0,
+                    "405": 0,
+                    "500": 0}
+
+try:
+    for line in sys.stdin:
+        parsed_line = line.split()  # ✄ trimming
+        parsed_line = parsed_line[::-1]  # inverting
+
+        if len(parsed_line) > 2:
+            counter += 1
+
+            if counter <= 10:
+                total_file_size += int(parsed_line[0])  # file size
+                code = parsed_line[1]  # status code
+
+                if (code in status_codes_dict.keys()):
+                    status_codes_dict[code] += 1
+
+            if (counter == 10):
+                print_message(status_codes_dict, total_file_size)
+                counter = 0
+
+finally:
+    print_message(status_codes_dict, total_file_size)
